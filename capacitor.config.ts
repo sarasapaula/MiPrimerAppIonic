64a2e43e3db38d4a2db54d8cc3c646947682326b
pruebalabs <pruebalabs@gmail.com>
2022-05-29 06:59:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mi_primer_app_Ionic',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
